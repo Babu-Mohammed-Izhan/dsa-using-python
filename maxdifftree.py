dce59951e3b097c9e299886f305d3ae38efcf15c
Izhan <babuizhan1234@gmail.com>
2021-12-31 20:55:41 +0530

    added max diff tree

---
+++ maxdifftree.py
@@ -0,0 +1,10 @@
+def maxAncestorDiff(self, root: Optional[TreeNode]) -> int:
+        self.res = 0
+        def helper(node, max_val, min_val):
+            if not node:
+                self.res = max(self.res, max_val - min_val)
+                return
+            helper(node.left, max(max_val, node.val), min(min_val, node.val))
+            helper(node.right, max(max_val, node.val), min(min_val, node.val))
+        helper(root, 0, 100000)
+        return self.res
