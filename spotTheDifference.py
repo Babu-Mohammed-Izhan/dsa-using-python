938e5f0e84fbf30cf65ec3d492467bc7eb37eacb
Izhan <babuizhan1234@gmail.com>
2022-01-03 19:21:04 +0530

    completed spot the difference

---
+++ spotTheDifference.py
@@ -0,0 +1,27 @@
+import unittest
+
+def spotTheDifference (s , t):
+    characters = {}
+    for i in s:
+        characters[i] = i
+
+    for i in t:
+        if i not in characters.keys():
+            return i
+
+    return ""
+
+
+class JewelsStones(unittest.TestCase):
+
+    def test1(self):
+        self.assertEqual(spotTheDifference('foobar', 'barfoot'), 't')
+    
+    def test2(self):
+        self.assertEqual(spotTheDifference('ide', 'idea'), 'a')
+    
+    def test3(self):
+        self.assertEqual(spotTheDifference('coding', 'ingcod'), '')
+
+if __name__ == '__main__':
+    unittest.main()
