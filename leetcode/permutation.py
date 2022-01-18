145f605acec012a7240757ca6984fb4b851af7f7
Izhan <babuizhan1234@gmail.com>
2022-01-18 13:29:32 +0530

    permutation dfs done

---
+++ leetcode/permutation.py
@@ -0,0 +1,30 @@
+# Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
+
+ 
+
+# Example 1:
+
+# Input: nums = [1,2,3]
+# Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+# Example 2:
+
+# Input: nums = [0,1]
+# Output: [[0,1],[1,0]]
+# Example 3:
+
+# Input: nums = [1]
+# Output: [[1]]
+
+
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        self.dfs(nums,[], res)
+        return res
+    
+    def dfs(self, nums, path, res):
+        if not nums:
+            res.append(path)
+        for i in range(len(nums)):
+            self.dfs(nums[:i]+nums[i+1:], path+[nums[i]], res)
