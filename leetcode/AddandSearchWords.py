9e5db4c49b99e1b98fcfd4a038100403572aa5ff
Izhan <babuizhan1234@gmail.com>
2022-01-28 21:28:18 +0530

    Added add binary search tree

---
+++ leetcode/AddandSearchWords.py
@@ -0,0 +1,45 @@
+# Design a data structure that supports adding new words and finding if a string matches any previously added string.
+
+# Implement the WordDictionary class:
+
+# WordDictionary() Initializes the object.
+# void addWord(word) Adds word to the data structure, it can be matched later.
+# bool search(word) Returns true if there is any string in the data structure that matches word or false otherwise. word may contain dots '.' where dots can be matched with any letter.
+ 
+
+
+
+class TrieNode():
+    def __init__(self):
+        self.children = collections.defaultdict(TrieNode)
+        self.isWord = False
+    
+class WordDictionary(object):
+    def __init__(self):
+        self.root = TrieNode()
+
+    def addWord(self, word):
+        node = self.root
+        for w in word:
+            node = node.children[w]
+        node.isWord = True
+
+    def search(self, word):
+        node = self.root
+        self.res = False
+        self.dfs(node, word)
+        return self.res
+    
+    def dfs(self, node, word):
+        if not word:
+            if node.isWord:
+                self.res = True
+            return 
+        if word[0] == ".":
+            for n in node.children.values():
+                self.dfs(n, word[1:])
+        else:
+            node = node.children.get(word[0])
+            if not node:
+                return 
+            self.dfs(node, word[1:])
