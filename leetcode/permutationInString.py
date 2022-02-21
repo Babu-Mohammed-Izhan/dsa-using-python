190c43d208ef5188523f0bb0ff2fc6ed7928e626
Izhan <babuizhan1234@gmail.com>
2022-02-21 21:29:08 +0530

    permutation in string

---
+++ leetcode/permutationInString.py
@@ -0,0 +1,36 @@
+# 567. Permutation in String
+
+# Given two strings s1 and s2, return true if s2 contains a permutation of s1, or false otherwise.
+
+# In other words, return true if one of s1's permutations is the substring of s2.
+
+ 
+
+# Example 1:
+
+# Input: s1 = "ab", s2 = "eidbaooo"
+# Output: true
+# Explanation: s2 contains one permutation of s1 ("ba").
+# Example 2:
+
+# Input: s1 = "ab", s2 = "eidboaoo"
+# Output: false
+
+
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        A = [ord(x) - ord('a') for x in s1]
+        B = [ord(x) - ord('a') for x in s2]
+
+        target = [0] * 26
+        for x in A:
+            target[x] += 1
+
+        window = [0] * 26
+        for i, x in enumerate(B):
+            window[x] += 1
+            if i >= len(A):
+                window[B[i - len(A)]] -= 1
+            if window == target:
+                return True
+        return False
