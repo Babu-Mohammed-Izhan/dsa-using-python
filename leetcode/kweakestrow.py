2ec9b9f632999f541fe32526edf16b1d1d7022f3
Izhan <babuizhan1234@gmail.com>
2022-03-27 19:45:32 +0530

    k weakest row fixed

--- leetcode/kweakestrow.py
+++ leetcode/kweakestrow.py
@@ -1,24 +1,24 @@
-1337. The K Weakest Rows in a Matrix
-Easy
+# 1337. The K Weakest Rows in a Matrix
+# Easy
 
-1997
+# 1997
 
-106
+# 106
 
-Add to List
+# Add to List
 
-Share
-You are given an m x n binary matrix mat of 1's (representing soldiers) and 0's (representing civilians). The soldiers are positioned in front of the civilians. That is, all the 1's will appear to the left of all the 0's in each row.
+# Share
+# You are given an m x n binary matrix mat of 1's (representing soldiers) and 0's (representing civilians). The soldiers are positioned in front of the civilians. That is, all the 1's will appear to the left of all the 0's in each row.
 
-A row i is weaker than a row j if one of the following is true:
+# A row i is weaker than a row j if one of the following is true:
 
-The number of soldiers in row i is less than the number of soldiers in row j.
-Both rows have the same number of soldiers and i < j.
-Return the indices of the k weakest rows in the matrix ordered from weakest to strongest.
+# The number of soldiers in row i is less than the number of soldiers in row j.
+# Both rows have the same number of soldiers and i < j.
+# Return the indices of the k weakest rows in the matrix ordered from weakest to strongest.
 
  
 
-Example 1:
+# Example 1:
 
 # Input: mat = 
 # [[1,1,0,0,0],
