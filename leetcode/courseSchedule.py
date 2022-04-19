cc1b94daf8335ebd0b6b52dd50e3d62f8c543f18
Izhan <babuizhan1234@gmail.com>
2022-04-19 18:08:28 +0530

    Completed course schedule done

---
+++ leetcode/courseSchedule.py
@@ -0,0 +1,46 @@
+# 207. Course Schedule
+# Medium
+
+# 9028
+
+# 363
+
+# Add to List
+
+# Share
+# There are a total of numCourses courses you have to take, labeled from 0 to numCourses - 1. You are given an array prerequisites where prerequisites[i] = [ai, bi] indicates that you must take course bi first if you want to take course ai.
+
+# For example, the pair [0, 1], indicates that to take course 0 you have to first take course 1.
+# Return true if you can finish all courses. Otherwise, return false.
+
+ 
+
+# Example 1:
+
+# Input: numCourses = 2, prerequisites = [[1,0]]
+# Output: true
+# Explanation: There are a total of 2 courses to take. 
+# To take course 1 you should have finished course 0. So it is possible.
+# Example 2:
+
+# Input: numCourses = 2, prerequisites = [[1,0],[0,1]]
+# Output: false
+# Explanation: There are a total of 2 courses to take. 
+# To take course 1 you should have finished course 0, and to take course 0 you should also have finished course 1. So it is impossible.
+
+
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        n = numCourses
+        G = [[] for i in range(n)]
+        degree = [0] * n
+        for j, i in prerequisites:
+            G[i].append(j)
+            degree[j] += 1
+        bfs = [i for i in range(n) if degree[i] == 0]
+        for i in bfs:
+            for j in G[i]:
+                degree[j] -= 1
+                if degree[j] == 0:
+                    bfs.append(j)
+        return len(bfs) == n
