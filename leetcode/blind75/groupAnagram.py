7a936b909db2dd7b191ee9bcc29c48599341dae5
Izhan <babuizhan1234@gmail.com>
2022-05-26 19:21:52 +0530

    Completed anagram

--- leetcode/blind75/groupAnagram.py
+++ leetcode/blind75/groupAnagram.py
@@ -1,3 +1,21 @@
+#? Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+
+#? An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+ 
+
+#? Example 1:
+
+#? Input: strs = ["eat","tea","tan","ate","nat","bat"]
+#? Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+
+
+#! Loop through the array of strings
+#! Sort the strings and place it in dictionary
+#! After sorting the strings, if the string is equal to a dict key,
+#! Then add string to dict key
+#! Return the list of all the dict values
+
 
 #! This function groups all the anagrams of a word together into arrays
 def groupAnagrams(strs):
